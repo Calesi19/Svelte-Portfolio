0b48a4a8b854438fb9fb7206ee9ef49229fce613
Carlos Lespin <carlos.lespin.silva@gmail.com>
2023-10-16 13:32:49 -0400

    fix: hero mobile alignment

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -19,10 +19,10 @@
 <div class="waveBackground w-full flex ">
     <!-- Your content here -->
     <div class="flex flex-col-reverse md:flex-row container items-center justify-between">
-        <div class="mt-8 mb-20 md:mb-0 md:mt-0">
+        <div class="mt-8 mb-20 md:mb-0 md:mt-0 flex flex-col md:items-start items-center">
             <h1 class="font-inter font-black text-[64px] md:text-[84px] text-[#1e1e1e] text-center md:text-start leading-none  ">Software<br> Developer</h1>
             <p class="font-inter font-bold text-[#505050] text-xl text-center md:text-start md:text-2xl mt-4">I’m a Software Engineering Student currently searching for internships.</p>
-            <button class="bg-[#1E1E1E] hover:bg-[#7b47af] active:bg-[#a55feb] active:duration-0 text-xl w-full md:w-auto mt-4 py-5 px-12 rounded-full text-white transition duration-300">Download Resume</button>
+            <button class="bg-[#1E1E1E] hover:bg-[#7b47af] active:bg-[#a55feb] active:duration-0 text-xl w-9/12 md:w-auto mt-4 py-5 px-12 rounded-full text-white transition duration-300">Download Resume</button>
         </div>
     
         
