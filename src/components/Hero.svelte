cf52496ef2233b606414dcebde1ea7cd4e056b53
Carlos Lespin <carlos.lespin.silva@gmail.com>
2023-10-16 13:47:50 -0400

    Update Hero.svelte

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -23,9 +23,9 @@
             <h1 class="font-inter font-black text-[64px] md:text-[84px] text-[#1e1e1e] text-center md:text-start leading-none  ">Software<br> Developer</h1>
             <p class="font-inter font-bold text-[#505050] text-xl text-center md:text-start md:text-2xl mt-4">I’m a Software Engineering Student currently searching for internships.</p>
             
-            <img class=" w-1/2 mt-5 md:hidden md:mt-4 lg:min-h-[500px] md:h-[50vh]" src="headframe.webp" alt="Carlos Lespin">
+            <img class=" w-2/3 mt-5 md:hidden md:mt-4 lg:min-h-[500px] md:h-[50vh]" src="headframe.webp" alt="Carlos Lespin">
 
-            <button class="bg-[#1E1E1E] hover:bg-[#7b47af] active:bg-[#a55feb] active:duration-0 text-xl w-9/12 md:w-auto mt-0 md:mt-4 py-5 px-12 rounded-full text-white transition duration-300">Download Resume</button>
+            <button class="bg-[#1E1E1E] hover:bg-[#7b47af] active:bg-[#a55feb] active:duration-0 text-xl w-full md:w-auto mt-0 md:mt-4 py-5 px-12 rounded-full text-white transition duration-300">Download Resume</button>
         </div>
     
         
