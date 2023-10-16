8ac750c9a67b81b5cf5b71320e9618e1ab5b6035
Carlos Lespin <carlos.lespin.silva@gmail.com>
2023-10-16 19:21:57 -0400

    fix: added resume section text

--- src/components/Resume.svelte
+++ src/components/Resume.svelte
@@ -28,11 +28,25 @@
 
         <img class="w-1/2 rounded-xl" src="Resume-Carlos.webp" alt="Resume">
             
-        <div class="flex flex-col h-full w-auto">
+        <div class="flex flex-col h-auto w-auto justify-center">
+
+            <h2 class="text-[32px] font-semibold">
+                Carlos Lespin
+            </h2>
+
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis nesciunt hic inventore facilis ea, ducimus quis sapiente ullam dignissimos quaerat omnis provident repellat animi dolorem labore commodi minima pariatur nihil.
+            </p>
+
             <button class="w-auto border-4 border-[#1e1e1e] bg-[#1e1e1e] text-xl mt-4 py-3 px-10 rounded-full text-white transition duration-300 active:duration-0 flex items-center gap-4">
                 <svg class="h-[24px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="Download"><path d="M8.29,13.29a1,1,0,0,0,0,1.42l3,3a1,1,0,0,0,1.42,0l3-3a1,1,0,0,0-1.42-1.42L13,14.59V3a1,1,0,0,0-2,0V14.59l-1.29-1.3A1,1,0,0,0,8.29,13.29ZM18,9H16a1,1,0,0,0,0,2h2a1,1,0,0,1,1,1v7a1,1,0,0,1-1,1H6a1,1,0,0,1-1-1V12a1,1,0,0,1,1-1H8A1,1,0,0,0,8,9H6a3,3,0,0,0-3,3v7a3,3,0,0,0,3,3H18a3,3,0,0,0,3-3V12A3,3,0,0,0,18,9Z" fill="#ffffff" class="color000000 svgShape"></path></svg>
-                Download PDF    
+                Resume PDF    
             </button>
+
+            <div class="w-full h-[300px] mt-5 rounded-lg bg-[#EAEAEA] border-2 border-[#fafafa] ">
+
+            </div>
+
 
         </div>
 
