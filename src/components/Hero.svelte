10a9dcc65b250fdf130372ec1f3c89e0f2604b37
Carlos Lespin <carlos.lespin.silva@gmail.com>
2023-10-18 00:02:19 -0400

    fix: hero button hover color

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -25,7 +25,7 @@
             
             <img class=" w-2/3 mt-5 md:hidden md:mt-4 lg:min-h-[500px] md:h-[50vh]" src="headframe.webp" alt="Carlos Lespin">
 
-            <button class="bg-[#1E1E1E] hover:bg-[#7b47af] active:bg-[#a55feb] active:duration-0 text-xl w-full md:w-auto mt-0 md:mt-4 py-4 px-12 rounded-full text-white transition duration-300">Download Resume PDF</button>
+            <button class="bg-[#1E1E1E] hover:bg-blue-500 active:bg-blue-600 active:duration-0 text-xl w-full md:w-auto mt-0 md:mt-4 py-4 px-12 rounded-full text-white transition duration-300">Download Resume PDF</button>
         </div>
     
         
