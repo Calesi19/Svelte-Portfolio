858f9d97059f06a3f362c6215dd5a9194dcb4735
Carlos Lespin <carlos.lespin.silva@gmail.com>
2023-10-17 23:22:47 -0400

    added colors to skill section

--- src/components/Skills.svelte
+++ src/components/Skills.svelte
@@ -7,7 +7,7 @@
     
     <div class="flex container gap-2 lg:justify-between mt-4 overflow-auto lg:overflow-auto no-scrollbar">
     
-        <div class="h-[250px] w-[200px] min-w-[200px] rounded-xl bg-[#EAEAEA] text-center flex flex-col px-10">
+        <div class="border-4 hover:border-purple-500 hover:bg-purple-50 h-[250px] w-[200px] min-w-[200px] rounded-xl bg-[#EAEAEA] text-center flex flex-col px-10">
             <svg class="h-14 mt-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="data-square"><path d="M17.2,7.75h3.6A1.953,1.953,0,0,0,22.75,5.8V4.2A1.953,1.953,0,0,0,20.8,2.25H17.2A1.953,1.953,0,0,0,15.25,4.2v.05H9.75V4A2.752,2.752,0,0,0,7,1.25H4A2.752,2.752,0,0,0,1.25,4V6A2.752,2.752,0,0,0,4,8.75H7A2.752,2.752,0,0,0,9.75,6V5.75h2V18a2.752,2.752,0,0,0,2.75,2.75h.75v.05a1.953,1.953,0,0,0,1.95,1.95h3.6a1.953,1.953,0,0,0,1.95-1.95V19.2a1.953,1.953,0,0,0-1.95-1.95H17.2a1.953,1.953,0,0,0-1.95,1.95v.05H14.5A1.252,1.252,0,0,1,13.25,18V13.25h2v.05a1.953,1.953,0,0,0,1.95,1.95h3.6a1.953,1.953,0,0,0,1.95-1.95V11.7A1.953,1.953,0,0,0,20.8,9.75H17.2a1.953,1.953,0,0,0-1.95,1.95v.05h-2v-6h2V5.8A1.953,1.953,0,0,0,17.2,7.75ZM16.75,4.2a.456.456,0,0,1,.45-.45h3.6a.456.456,0,0,1,.45.45V5.8a.456.456,0,0,1-.45.45H17.2a.456.456,0,0,1-.45-.45ZM8.25,6A1.252,1.252,0,0,1,7,7.25H4A1.252,1.252,0,0,1,2.75,6V4A1.252,1.252,0,0,1,4,2.75H7A1.252,1.252,0,0,1,8.25,4Zm8.5,13.2a.456.456,0,0,1,.45-.45h3.6a.456.456,0,0,1,.45.45v1.6a.456.456,0,0,1-.45.45H17.2a.456.456,0,0,1-.45-.45Zm0-7.5a.456.456,0,0,1,.45-.45h3.6a.456.456,0,0,1,.45.45v1.6a.456.456,0,0,1-.45.45H17.2a.456.456,0,0,1-.45-.45Z" data-name="1"></path></svg>
     
             <p class="mt-10 font-inter font-bold text-lg">
@@ -16,7 +16,7 @@
     
         </div>
     
-        <div class="h-[250px] w-[200px] min-w-[200px] rounded-xl bg-[#EAEAEA] text-center flex flex-col px-10">
+        <div class="border-4 hover:border-green-500 hover:bg-green-50 h-[250px] w-[200px] min-w-[200px] rounded-xl bg-[#EAEAEA] text-center flex flex-col px-10">
             <svg class="h-14 mt-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="software"><path d="M18 3H6C3.8 3 2 4.8 2 7v10c0 2.2 1.8 4 4 4h12c2.2 0 4-1.8 4-4V7c0-2.2-1.8-4-4-4zm2 14c0 1.1-.9 2-2 2H6c-1.1 0-2-.9-2-2v-7h16v7zm0-9H4V7c0-1.1.9-2 2-2h12c1.1 0 2 .9 2 2v1z"></path><circle cx="10.9" cy="6.6" r=".8"></circle><circle cx="8.3" cy="6.6" r=".8"></circle><circle cx="5.8" cy="6.6" r=".8"></circle></svg>      
                 
                 <p class="mt-10 font-inter font-bold text-lg">
@@ -26,7 +26,7 @@
         </div>
     
     
-        <div class="h-[250px] w-[200px] min-w-[200px] rounded-xl bg-[#EAEAEA] text-center flex flex-col px-10">
+        <div class="border-4 hover:border-yellow-500 hover:bg-yellow-50 h-[250px] w-[200px] min-w-[200px] rounded-xl bg-[#EAEAEA] text-center flex flex-col px-10">
             <svg class="h-14 mt-10" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 24 24" id="mortarboard"><path d="M21.49,10.19l-1-.55h0l-9-5-.11,0a1.06,1.06,0,0,0-.19-.06l-.19,0-.18,0a1.17,1.17,0,0,0-.2.06l-.11,0-9,5a1,1,0,0,0,0,1.74L4,12.76V17.5a3,3,0,0,0,3,3h8a3,3,0,0,0,3-3V12.76l2-1.12V14.5a1,1,0,0,0,2,0V11.06A1,1,0,0,0,21.49,10.19ZM16,17.5a1,1,0,0,1-1,1H7a1,1,0,0,1-1-1V13.87l4.51,2.5.15.06.09,0a1,1,0,0,0,.25,0h0a1,1,0,0,0,.25,0l.09,0a.47.47,0,0,0,.15-.06L16,13.87Zm-5-3.14L4.06,10.5,11,6.64l6.94,3.86Z"></path></svg>       
            
             <p class="mt-10 font-inter font-bold text-lg">
@@ -36,7 +36,7 @@
         </div>
     
     
-        <div class="h-[250px] w-[200px] min-w-[200px] rounded-xl bg-[#EAEAEA] text-center flex flex-col px-10">
+        <div class="border-4 hover:border-blue-500 hover:bg-blue-50 h-[250px] w-[200px] min-w-[200px] rounded-xl bg-[#EAEAEA] text-center flex flex-col px-10">
             <svg class="h-14 mt-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="phone"><path d="M15 2H9C6.8 2 5 3.8 5 6v12c0 2.2 1.8 4 4 4h6c2.2 0 4-1.8 4-4V6c0-2.2-1.8-4-4-4zm2 16c0 1.1-.9 2-2 2H9c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2h6c1.1 0 2 .9 2 2v12z"></path><path d="M13 5h-2c-.6 0-1 .4-1 1s.4 1 1 1h2c.6 0 1-.4 1-1s-.4-1-1-1z"></path></svg>
             <p class="mt-10 font-inter font-bold text-lg">
                 Mobile App Development
@@ -45,7 +45,7 @@
         </div>
     
     
-        <div class="h-[250px] w-[200px] min-w-[200px] rounded-xl bg-[#EAEAEA] text-center flex flex-col px-10">
+        <div class="border-4 hover:border-red-500 hover:bg-red-50 h-[250px] w-[200px] min-w-[200px] rounded-xl bg-[#EAEAEA] text-center flex flex-col px-10">
             <svg class="h-14 mt-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="code"><rect width="256" height="256" fill="none"></rect><polyline fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="24" points="64 88 16 128 64 168"></polyline><polyline fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="24" points="192 88 240 128 192 168"></polyline><line x1="160" x2="96" y1="40" y2="216" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line></svg>        <p class="mt-10 font-inter font-bold text-lg">
                 Web Development
             </p>
