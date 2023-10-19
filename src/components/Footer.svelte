18ab5daef93cd0b2b559d1f53e7af4b2a2de663b
Carlos Lespin <carlos.lespin.silva@gmail.com>
2023-10-18 21:28:04 -0400

    feature: call option on mobile footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -12,6 +12,12 @@
         
         <div >
 
+            <a class="md:hidden" href="tel:+7879889447">
+                <button class="hover:scale-105 hover:border-[#ffcc56] active:bg-[#ffcc56] w-[300px] border-4 border-[#EAEAEA]  text-xl mt-4 py-3 px-10 rounded-full text-white transition duration-300 active:duration-0  flex items-center gap-4">
+                    <svg class="w-[32px] h-[32px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="Message"><g data-name="Layer 2" fill="#ffffff" class="color000000 svgShape"><g data-name="message-circle" fill="#ffffff" class="color000000 svgShape"><circle cx="12" cy="12" r="1" fill="#ffffff" class="color000000 svgShape"></circle><circle cx="16" cy="12" r="1" fill="#ffffff" class="color000000 svgShape"></circle><circle cx="8" cy="12" r="1" fill="#ffffff" class="color000000 svgShape"></circle><path d="M19.07 4.93a10 10 0 0 0-16.28 11 1.06 1.06 0 0 1 .09.64L2 20.8a1 1 0 0 0 .27.91A1 1 0 0 0 3 22h.2l4.28-.86a1.26 1.26 0 0 1 .64.09 10 10 0 0 0 11-16.28zm.83 8.36a8 8 0 0 1-11 6.08 3.26 3.26 0 0 0-1.25-.26 3.43 3.43 0 0 0-.56.05l-2.82.57.57-2.82a3.09 3.09 0 0 0-.21-1.81 8 8 0 0 1 6.08-11 8 8 0 0 1 9.19 9.19z" fill="#ffffff" class="color000000 svgShape"></path></g></g></svg>                    Text Me
+                </button>
+                </a>
+
             <a href="mailto:carlos.lespin.silva@gmail.com">
             <button class="hover:scale-105 hover:border-[#ffcc56] active:bg-[#ffcc56] w-[300px] border-4 border-[#EAEAEA]  text-xl mt-4 py-3 px-10 rounded-full text-white transition duration-300 active:duration-0  flex items-center gap-4">
                 <svg class="w-[32px] h-[32px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="Email"><g data-name="Layer 2" fill="#ffffff" class="color000000 svgShape"><path d="M19 4H5a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3zm-.67 2L12 10.75 5.67 6zM19 18H5a1 1 0 0 1-1-1V7.25l7.4 5.55a1 1 0 0 0 .6.2 1 1 0 0 0 .6-.2L20 7.25V17a1 1 0 0 1-1 1z" data-name="email" fill="#ffffff" class="color000000 svgShape"></path></g></svg>
