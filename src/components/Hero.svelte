506c7d150386461adbcf68116d120aa4b7b91f1e
Carlos Lespin <carlos.lespin.silva@gmail.com>
2023-10-18 21:54:21 -0400

    fix: added name to link for accessibility

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -26,6 +26,7 @@
             -->
 
             <a href="/documents/Resume-Carlos.pdf" download class="w-11/12">
+              <span class="hidden">Download Resume PDF</span>
               <button class="flex mt-4 md:hidden items-center gap-5 bg-blue-600 hover:bg-blue-700 active:bg-blue-600 active:duration-0 text-xl w-full md:w-auto md:mt-4 py-4 px-12 rounded-full text-white transition duration-300">
                 <svg class="h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="Download"><path d="M8.29,13.29a1,1,0,0,0,0,1.42l3,3a1,1,0,0,0,1.42,0l3-3a1,1,0,0,0-1.42-1.42L13,14.59V3a1,1,0,0,0-2,0V14.59l-1.29-1.3A1,1,0,0,0,8.29,13.29ZM18,9H16a1,1,0,0,0,0,2h2a1,1,0,0,1,1,1v7a1,1,0,0,1-1,1H6a1,1,0,0,1-1-1V12a1,1,0,0,1,1-1H8A1,1,0,0,0,8,9H6a3,3,0,0,0-3,3v7a3,3,0,0,0,3,3H18a3,3,0,0,0,3-3V12A3,3,0,0,0,18,9Z" fill="#ffffff" class="color000000 svgShape"></path></svg>
                 Download Resume</button>
