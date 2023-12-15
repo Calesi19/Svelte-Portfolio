3883e602d3b9462512ca28d28408867196ff3acd
Carlos Lespin <carlos.lespin.silva@gmail.com>
2023-12-15 14:43:30 -0500

    misc: started new project section draft

---
+++ src/components/Projects2.svelte
@@ -0,0 +1,48 @@
+
+
+<style>
+    .waveBackgroundMiddle {
+      background: url('/waves-middle.svg') no-repeat center center;
+      background-size: cover; /* This makes sure the SVG covers the entire div */
+      height: auto;
+      min-height: 50vh;
+      width: 100%;
+    }
+</style>
+
+
+<div class="waveBackgroundMiddle min-h-[800px] py-[100px] " >
+
+
+    <div class="flex flex-col mt-4 gap-14 md:gap-4">
+
+
+        <div class="container flex flex-col md:flex-row justify-between gap-4 md:gap-10">
+
+            <img class="w-full md:w-1/3 rounded-xl transition duration-300 hover:scale-105 hover:shadow-lg" src="LockerHub.webp" alt="LockerHub">
+
+
+        </div>
+
+        <div class="container flex flex-col md:flex-row justify-between gap-4 md:gap-10">
+
+            <img class="w-full md:w-1/3 rounded-xl transition duration-300 hover:scale-105 hover:shadow-lg" src="Timeshare-Banner.webp" alt="Timeshare Pro">
+
+
+            <div class="flex flex-col gap-2 h-auto justify-center">
+                
+                
+
+
+            </div>
+            
+
+
+        </div>
+
+        <div class="container flex flex-col md:flex-row justify-between gap-4 md:gap-10">
+            <img class="w-full md:w-1/3 rounded-xl transition duration-300 hover:scale-105 hover:shadow-lg" src="Orbital-Simulator.webp" alt="Timeshare Pro">
+        </div>
+    </div>
+
+</div>
