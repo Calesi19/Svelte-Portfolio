f89e0cbd4a9fb3218ef189eb819fb0faabdb866a
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-01-31 22:18:44 -0500

    fix: updated resume

--- src/components/Resume2.svelte
+++ src/components/Resume2.svelte
@@ -1,5 +1,8 @@
 <script>
-   
+
+import { Lightbox } from 'svelte-lightbox'
+
+
 
 </script>
 
@@ -27,11 +30,12 @@
 
     <div class="container flex flex-col md:flex-row mt-5 gap-10">
 
-
+<Lightbox>
         
+  
             <img class="md:w-1/2 rounded-xl shadow-lg hover:shadow-2xl hover:scale-105 transition duration-1000 cursor-pointer" src="resume.webp" alt="Resume">
 
-
+</Lightbox>
 
             
         <div class="flex flex-col h-auto w-auto justify-center">
@@ -51,7 +55,7 @@
 
             <div class="flex flex-col w-full xl:flex-row lg:gap-4">
 
-                <a href="/documents/Resume.pdf" download>
+                <a href="/documents/Resume-Carlos.pdf" download>
                     <button class="w-full bg-blue-600 hover:bg-blue-700 active:bg-blue-600 text-lg mt-4 py-4 md:py-3 px-10 rounded-full text-white transition duration-300 active:duration-0 flex items-center gap-4">
                         <svg class="h-[24px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="Download"><path d="M8.29,13.29a1,1,0,0,0,0,1.42l3,3a1,1,0,0,0,1.42,0l3-3a1,1,0,0,0-1.42-1.42L13,14.59V3a1,1,0,0,0-2,0V14.59l-1.29-1.3A1,1,0,0,0,8.29,13.29ZM18,9H16a1,1,0,0,0,0,2h2a1,1,0,0,1,1,1v7a1,1,0,0,1-1,1H6a1,1,0,0,1-1-1V12a1,1,0,0,1,1-1H8A1,1,0,0,0,8,9H6a3,3,0,0,0-3,3v7a3,3,0,0,0,3,3H18a3,3,0,0,0,3-3V12A3,3,0,0,0,18,9Z" fill="#ffffff" class="color000000 svgShape"></path></svg>
                         Resume PDF    
